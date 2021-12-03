c5416930ded98199eced73ad6a521a3dcb7822e8
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-12-03 11:22:03 +0000

    better var names

--- 2021/Day1/Part1.py
+++ 2021/Day1/Part1.py
@@ -2,20 +2,20 @@
 
 
 with open('2021/Day1/input-advent-1.txt') as f:
-    lines = f.readlines()
+    input_lines = f.readlines()
 
 # Get rid of newlines & convert to numbers
 
-newText = []
-for line in lines:
-    newLine = int(line.replace('\n', ''))
-    newText.append(newLine)
+depths_list = []
+for line in input_lines:
+    depth_num = int(line.replace('\n', ''))
+    depths_list.append(depth_num)
 
 # Iterate through the list and add 1 to the counter when n>n-1
 
-result = 0
-for i in range(1, len(newText)):
-    if newText[i] > newText[i-1]:
-        result +=1
+num_greater_than_previous = 0
+for i in range(1, len(depths_list)):
+    if depths_list[i] > depths_list[i-1]:
+        num_greater_than_previous +=1
 
-print(result)
+print(num_greater_than_previous)
