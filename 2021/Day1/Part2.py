e1057421ceeb69ff8d57e33fee78dab72410b749
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-12-03 11:24:22 +0000

    add part 2

---
+++ 2021/Day1/Part2.py
@@ -0,0 +1,21 @@
+# Get the text from the input
+
+
+with open('2021/Day1/input-advent-1.txt') as f:
+    input_lines = f.readlines()
+
+# Get rid of newlines & convert to numbers
+
+depths_list = []
+for line in input_lines:
+    depth_num = int(line.replace('\n', ''))
+    depths_list.append(depth_num)
+
+# Iterate through the list and add 1 to the counter when the sliding window sum is greater than the previous one
+
+num_window_greater_than_previous = 0
+for i in range(1, len(depths_list)):
+    if (depths_list[i]+depths_list[i+1]+depths_list[i+2]) > (depths_list[i-1]+depths_list[i]+depths_list[i+1]):
+        num_window_greater_than_previous +=1
+
+print(num_window_greater_than_previous)
