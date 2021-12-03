5dd115139a0d687abdbe336ca567cb5a0988fd41
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2021-12-03 10:30:15 +0000

    get method down

---
+++ 2021/Day1/Part1.js
@@ -0,0 +1,13 @@
+// Use python
+
+// Scrape the content of this page https://adventofcode.com/2021/day/1/input
+
+// Turn it into a list
+
+// Iterate through the list comparing n with n+1
+
+// Tick a  counter variable when n+1 > n
+
+// Return counter
+
+// Profit
